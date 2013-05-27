01a446e7ee0808033fb7fa3b7adb983d1c2a5011
Adrian Halfdan Ulland <PlayPrey@Gmail.com>
2013-05-27 21:29:56 +0200

    Update 3
    Third update

--- MoRe_common/com/PlayPrey/MoRe/item/ModItems.java
+++ MoRe_common/com/PlayPrey/MoRe/item/ModItems.java
@@ -1,6 +1,12 @@
 package com.PlayPrey.MoRe.item;
+import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.ItemStack;
 
+import com.PlayPrey.MoRe.block.ModBlocks;
 import com.PlayPrey.MoRe.lib.ItemIDs;
+
+import cpw.mods.fml.common.registry.GameRegistry;
 
 import net.minecraft.item.Item;
 
@@ -13,5 +19,10 @@ public class ModItems
 	{
 		DiamondNugget = new ItemDiamondNugget(ItemIDs.DIAMONDNUGGET_ID);
 	}
-	
+	public static void recipesInit(){
+
+		GameRegistry.addRecipe(new ItemStack(Item.diamond,1,0), new Object[]{
+			"DDD","DDD","DDD",'D', ModItems.DiamondNugget
+		});
+	}
 }
